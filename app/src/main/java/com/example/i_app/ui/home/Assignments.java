84b8915343eac8f45ea62f49d3a317dad1f965a0
Ayushman <ayushmanraghav.ra@gmail.com>
2020-04-14 15:37:18 +0530

    ACTIVITIES ADDED TO NAVIGATION DRAWER

---
+++ app/src/main/java/com/example/i_app/ui/home/Assignments.java
@@ -0,0 +1,16 @@
+package com.example.i_app.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.i_app.R;
+
+public class Assignments extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_assignments);
+    }
+}
---
+++ app/src/main/java/com/example/i_app/ui/home/Notes.java
@@ -0,0 +1,16 @@
+package com.example.i_app.ui.home;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.i_app.R;
+
+public class Notes extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notes);
+    }
+}
