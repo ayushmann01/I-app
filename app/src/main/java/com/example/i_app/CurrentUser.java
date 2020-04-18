125e0b0becf5e207313d5c879bcf6b100ea482f7
Ayushman <ayushmanraghav.ra@gmail.com>
2020-04-19 01:36:07 +0530

    class holds current User details

---
+++ app/src/main/java/com/example/i_app/CurrentUser.java
@@ -0,0 +1,43 @@
+package com.example.i_app;
+
+import android.widget.ImageView;
+
+public class CurrentUser {
+    private String username;
+    private ImageView userImage;
+    private String userEmail;
+
+    public CurrentUser(String username, ImageView userImage, String userEmail) {
+        this.username = username;
+        this.userImage = userImage;
+        this.userEmail = userEmail;
+    }
+
+    public CurrentUser(){
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public ImageView getUserImage() {
+        return userImage;
+    }
+
+    public void setUserImage(ImageView userImage) {
+        this.userImage = userImage;
+    }
+
+    public String getUserEmail() {
+        return userEmail;
+    }
+
+    public void setUserEmail(String userEmail) {
+        this.userEmail = userEmail;
+    }
+}
